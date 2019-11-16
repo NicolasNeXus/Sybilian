81e97d891a07aae32afb7e9b002116d113d770b5
Nicolas Birac <nicolas.birac@gmail.com>
2019-11-16 16:53:25 +0100

    Working basic version of the game with hand, life, purgatory, graveyard and attack of monster

--- sybilian/cards.py
+++ sybilian/cards.py
@@ -1,24 +1,42 @@
-class Cards():
-    def __init__(self, name : str, price : int, color : str, effect : str):
+class Card():
+    """
+        A card has a name, a price
+        a color and an effect that
+        is stored as a dict
+    """
+    def __init__(self, name : str, price : int, color : str, effect : str, owner : str = None) -> None:
         self.name = name
         self.price = price
         self.color = color
         self.effect = effect
+        self.owner = owner
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+            Create a string from
+            a card
+        """
         printable_content = self.__class__.__name__ + " | "
         printable_content+= "Name : "+self.name+ " | "
         printable_content+= "Price : "+str(self.price) + " | "
         printable_content+= "Effect : "+self.effect
         return printable_content
 
-class Monster(Cards):
-    def __init__(self, name : str, price : int, color : str, kin : str, effect : dict):
-        super().__init__(name, price, color, effect)
+class Monster(Card):
+    """
+        A monster is a card that cost 1
+        and has a kin
+    """
+    def __init__(self, name : str, price : int, color : str, kin : str, effect : dict, owner : str = None) -> None:
+        super().__init__(name, price, color, effect, owner)
         self.kin = kin
         self.life = 2
+        self.coord = (0,0)
 
     def attack(self, card_2):
+        """
+            A card can attack an other card
+        """
         assert(self.life > 0)
         if effect["Puissante"]==1:
             if card_2.effect["Puissante"]==1:
@@ -33,12 +51,21 @@ class Monster(Cards):
                 life-=1
             card_2.life-=1
 
-class Spell(Cards):
-    def __init__(self, name : str, price : int, color : str, effect : dict):
-        super().__init__(name, price, color, effect)
+class Spell(Card):
+    """
+        A spell is a card that cost 0
+        and that has effects.
+    """
+    def __init__(self, name : str, price : int, color : str, effect : dict, owner : str = None) -> None:
+        super().__init__(name, 0, color, effect, owner)
 
-class Counterspell(Cards):
-    def __init__(self, name : str, price : int, color : str, effect : dict):
-        super().__init__(name, price, color, effect)
+class Counterspell(Card):
+    """
+        A Counterspell is a card that
+        cost 0 but that can only be played
+        when the other player plays a card
+    """
+    def __init__(self, name : str, price : int, color : str, effect : dict, owner : str = None) -> None:
+        super().__init__(name, 0, color, effect, owner)
 
 
--- sybilian/deck.py
+++ sybilian/deck.py
@@ -4,15 +4,17 @@ import csv
 from cards import *
 
 class Deck:
-    def __init__(self, my_container):
+    def __init__(self, my_container : list, owner : str = None) -> None:
         """
             Create a Deck of cards
             with a LIFO
         """
+        for card in my_container:
+            card.owner = owner
         self.container = deque(my_container)
         self.size = len(my_container)
 
-    def draw(self):
+    def draw(self) -> Card:
         """
             Draw a card from the deck
             and return the card
@@ -20,21 +22,21 @@ class Deck:
         self.size-=1
         return self.container.pop()
 
-    def add_end(self, card):
+    def add_end(self, card : Card) -> None:
         """
             Add a card at the end of the deck
         """
         self.container.appendleft(card)
         self.size+=1
 
-    def add(self, card):
+    def add(self, card : Card) -> None:
         """
             Add a card to the deck
         """
         self.container.append(card)
         self.size+=1
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         """
             Shuffle the Deck
         """
@@ -42,7 +44,7 @@ class Deck:
         random.shuffle(list_container)
         self.container = deque(list_container)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
             Print a deck
         """
@@ -50,28 +52,50 @@ class Deck:
         for cards in list(self.container):
             printable_content+=cards.__str__()+"\n"
         return printable_content
-class Hand(Deck):
-    def __init__(self, deck : Deck):
+
+
+class Life(Deck):
+    def __init__(self, deck : Deck) -> None:
         """
             Keep a copy of the deck
             within the hand. Linker.
             Maxlen = 10 ?
         """
-        self.container = deque([])
+        super().__init__([])
         self.deck = deck
-        self.size = 0
+        for i in range(9):
+            self.add(deck.draw())
+
+class Hand(Deck):
+    def __init__(self, deck : Deck, life : Life) -> None:
+        """
+            Keep a copy of the deck
+            within the hand. Linker.
+            Maxlen = 10 ?
+        """
+        super().__init__([])
+        self.deck = deck
+        self.life = life
         for i in range(4):
             self.add(deck.draw())
 
-    def draw(self):
+    def draw(self) -> None:
         """
-            add the drawn card to the hand
+            add the drawn card to the hand.
+            The card comes from the deck
         """
         self.add(self.deck.draw())
 
-    def play(self, j):
+    def draw_hp(self) -> None:
         """
-            play the j-th card of the game
+            add the drawn card to the hand.
+            The card comes from the health
+        """
+        self.add(self.life.draw())
+
+    def play(self, j : int) -> None:
+        """
+            play the j-th card of the hand
         """
         self.size-=1
         storage = deque([])
@@ -81,9 +105,28 @@ class Hand(Deck):
         for i in range(j):
             self.container.append(storage.pop())
         return card_j
-        
 
-def csv_to_deck(csv_file):
+class Graveyard(Deck):
+    def __init__(self) -> None:
+        super().__init__([])
+
+    def grab(self, j : int) -> Card:
+        """
+            grab the jth card
+            form the Graveyard
+        """
+        self.size-=1
+        storage = deque([])
+        for i in range(j):
+            storage.append(self.container.pop())
+        card_j = self.container.pop()
+        for i in range(j):
+            self.container.append(storage.pop())
+        return card_j
+
+
+
+def csv_to_deck(csv_file : str) -> Deck:
     """
         Basic function to create a deck
         from a csv file. Used for dev only
@@ -107,20 +150,9 @@ def csv_to_deck(csv_file):
                     if i==3:
                         effect = cells
                 if type_card == "Sort":
-                    deck.add(Spell(name,0,color, effect))
+                    deck.add(Spell(name,0,color, effect, csv_file))
                 if type_card == "Créature":
-                    deck.add(Monster(name,1,color,"",effect))
+                    deck.add(Monster(name,1,color,"",effect, csv_file))
                 if type_card == "Parade":
-                    deck.add(Counterspell(name,0,color,effect))
+                    deck.add(Counterspell(name,0,color,effect, csv_file))
     return deck
-
-
-
-if __name__=="__main__":
-    card_1 = Monster("Devorombre", 1, "Violet", "Demon", "Transforme en ombre la créature attaquée")
-    card_2 = Counterspell("Armistice", 0, "Gris","Toutes les prochaines attaques sont contrés")
-    card_3 = Spell("Boule de feu", 0, "Rouge", "L'adversaire perd un point de vie")
-    deck = Deck([card_1, card_2, card_3])
-    deck_2 = csv_to_deck("draft_bleu.csv")
-
-
