25b0f98336895c903b1f9af40ff23b3d39e19021
clarinevong <55847721+clarinevong@users.noreply.github.com>
2019-11-27 16:32:41 +0100

    lignes utilisées par le joueur

--- sybilian/main.py
+++ sybilian/main.py
@@ -4,10 +4,13 @@ from board import *
 from player import *
 
 board = Board()
-player_one = Player("draft_bleu.csv", board)
-player_two = Player("draft_rouge.csv", board)
+# player_one has access to the first and second lines of the board
+# player_two has access to the third and fourth lines of the board
+player_one = Player("draft_bleu.csv", board, [0, 1], 2)
+player_two = Player("draft_rouge.csv", board, [2, 3], 1)
 player_one.other_player = player_two
 player_two.other_player = player_one
+
 
 player_one.play(2,(1,0))
 player_two.play(2,(3,0))
