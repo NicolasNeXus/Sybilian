a1d75cd1d2e6c29a1d491ecce90afa13eb99bce9
Paul-Vinh <44447840+Paul-Vinh@users.noreply.github.com>
2020-01-06 15:40:17 +0100

    Graphics changed

--- sybilian/main.py
+++ sybilian/main.py
@@ -3,27 +3,12 @@ from deck import *
 from board import *
 from player import *
 from game import *
-from PyQt5.QtWidgets import QPushButton, QWidget, QApplication, QLabel
-from PyQt5.QtCore import Qt, QMimeData, QRect
-from PyQt5.QtGui import QPainter, QColor, QPixmap, QDrag, QPen, QCursor
-
-<<<<<<< HEAD
+from PyQt5.QtWidgets import QApplication
+import sys
             
 if __name__ == '__main__':
   
     app = QApplication(sys.argv)
     game = Game()
-    game.game_loop()
     game.show()
     app.exec_()
-=======
-game = Game()
-game.game_loop()
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> 15ddc9ad5a33ea0bbe88c16066fb9ce1e8c6c7f5
->>>>>>> 7a568159ae5ff9b47a276438579ff253618876fc
