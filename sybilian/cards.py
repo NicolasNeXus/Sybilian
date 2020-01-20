dcc7b1f9cb8318bd31e08d60783238b2a71d12dd
Paul-Vinh <44447840+Paul-Vinh@users.noreply.github.com>
2020-01-20 18:02:54 +0100

    Partie Graphique

--- sybilian/cards.py
+++ sybilian/cards.py
@@ -2,39 +2,21 @@ from PyQt5.QtWidgets import QPushButton, QWidget, QApplication, QLabel
 from PyQt5.QtCore import Qt, QMimeData, QRect
 from PyQt5.QtGui import QPainter, QColor, QPixmap, QDrag, QPen, QCursor
 
-class Card(QLabel):
+class Card_Graphic(QLabel):
     """
         A card has a name, a price
         a color and an effect that
         is stored as a dict
     """
-    #def __init__(self, name : str, price : int, color : str, effect : dict, game_text : str, owner : str = None) -> None:
-    def __init__(self, name : str, parent, price : int, color : str, effect : dict, game_text : str, owner : str = None) -> None:
-        super().__init__(name, parent)
-        self.name = name
-        self.price = price
-        self.color = color
-        self.effect = effect
-        self.owner = owner
-        self.game_text = game_text
+    def __init__(self, card: Card, parent: QWidget) -> None:
+        super().__init__(card.name, parent)
+        self.card = card
         self.setAcceptDrops(True)
         self.explanation = QLabel('', parent)
-        self.parent = None # stuck to which area
         self.win = parent # parent window
         self.image = "monster.png" # card representation
         self.width = 50
         self.height = 70
-
-    def __str__(self) -> str:
-        """
-            Create a string from
-            a card
-        """
-        printable_content = self.__class__.__name__ + " | "
-        printable_content+= "Name : "+self.name+ " | "
-        printable_content+= "Price : "+str(self.price) + " | "
-        printable_content+= "Game Text : "+self.game_text
-        return printable_content
 
     def mouseMoveEvent(self, e):
         if e.buttons() != Qt.LeftButton:
@@ -46,77 +28,10 @@ class Card(QLabel):
         drag.setHotSpot(e.pos() - self.rect().topLeft())
         dropAction = drag.exec_(Qt.MoveAction)
     
-    def enterEvent(self, e):  
+    def enterEvent(self, e):
         self.explanation.move(680, 400)
-        self.explanation.setText(self.name + "\n\n" + self.game_text)
+        self.explanation.setText(self.card.name + "\n\n" + self.card.game_text)
         self.explanation.show()
-        
-        # invalid cards : they have been already used
-        #if self.placed:
-            #self.win.setOverrideCursor(Qt.ForbiddenCursor)
         
     def leaveEvent(self, e):
         self.explanation.hide()
-        
-#        if self.placed:
-#            self.win.restoreOverrideCursor()
-
-class Placeholder(Card):
-    """
-        A object used for the
-        interface. It's a
-        placeholder for cards.
-    """
-    def __init__(self):
-        self.name = "placeholder"
-        self.price = 0
-        self.effect = {}
-        self.game_text = " . "
-        self.image = ""
-        
-
-class Monster(Card):
-    """
-        A monster is a card that cost 1
-        and has a kin
-    """
-    def __init__(self, name : str, parent : QWidget, price : int, color : str, kin : str, effect : dict, game_text : str, owner : str = None) -> None:
-        super().__init__(name, parent, price, color, effect, game_text, owner)
-        self.kin = kin
-        self.life = 2
-        self.coord = (0,0)
-
-    def attack(self, card_2):
-        """
-            A card can attack an other card
-        """
-        assert(self.life > 0)
-        if "Powerful" in self.effect.keys() and self.effect["Powerful"]=="No_condition":
-            if "Powerful" in card_2.effect.keys() and card_2.effect["Powerful"]=="No_condition":
-                card_2.life-=1
-            else:
-                card_2.life = 0
-            self.life-=1
-        else:
-            if "Powerful" in card_2.effect.keys() and card_2.effect["Powerful"]=="No_condition":
-                self.life = 0
-            else:
-                self.life-=1
-            card_2.life-=1
-
-class Spell(Card):
-    """
-        A spell is a card that cost 0
-        and that has effects.
-    """
-    def __init__(self, name : str, parent : QWidget, price : int, color : str, effect : dict, game_text : str, owner : str = None) -> None:
-        super().__init__(name, parent, 0, color, effect, game_text, owner)
-
-class Counterspell(Card):
-    """
-        A Counterspell is a card that
-        cost 0 but that can only be played
-        when the other player plays a card
-    """
-    def __init__(self, name : str, parent : QWidget, price : int, color : str, effect : dict, game_text : str, owner : str = None) -> None:
-        super().__init__(name, parent, 0, color, effect, game_text, owner)
--- sybilian/game.py
+++ sybilian/game.py
@@ -21,138 +21,207 @@ import sys
 class Game(QWidget):
     def __init__(self, app : QApplication) -> None:
         super().__init__()
-        self.board = Board()
-        # player_one has access to the first and second lines of the board
-        # player_two has access to the third and fourth lines of the board
-        self.players = (Player("deckA005.csv", self.board, [0, 1], 2, self), Player("deckB001.csv", self.board, [2, 3], 1, self))
-        self.players[0].other_player = self.players[1]
-        self.players[1].other_player = self.players[0]
-        self.nb_turns = 0
-        self.nb_actions = 2 # possible actions during a turn
         self.end = False
         self.ongo_attack = False
-        
-        
+        self.current_hand = [Placeholder() for i in range(4)]
+        self.other_hand = [Placeholder() for i in range(4)]
+        self.current_life = []
+        self.other_life = []
+        self.game = GameControl()
+        self.tour = 0
+        self.current_deck = Card_Graphic(Placeholder(), app) 
+        self.other_deck = Card_Graphic(Placeholder(), app)
+        self.i_card_attacking, self.j_card_attacking = -1, -1
+        #self.i_card_attacked, self.j_card_attacked = -1, -1
+
         self.finish_turn = QPushButton('FINISH YOUR TURN', self) # button to finish your turn
         self.setMouseTracking(True) # mouse tracking on the screen
         self.setAcceptDrops(True)
-        self.initUi()
-        self.position = [[QRect(150*j + 100, 100 + 150 * i, 50, 100) for j in range(3)] for i in range(2)] + [[QRect(150*j + 100, 400 + 150 * i, 50, 100) for j in range(3)] for i in range(2)]
-        self.color = [[QColor(255, 0, 0, 200) for j in range(3)] for i in range(2)] + [[QColor(0, 0, 255, 200) for j in range(3)] for i in range(2)]
-        self.grid = [[Placeholder for j in range(3)] for i in range(2)] + [[Placeholder for j in range(3)] for i in range(2)]
-        self.name_player = [["Joueur1" for j in range(3)]for i in range(2)] + [["Joueur2" for j in range(3)]for i in range(2)]
-        self.initHand()
-        self.initLife()
         self.index_current_card = -1
         self.qapp = app
         self.player_medal = [QRect(0, 0, 100, 50), QRect(0, 850, 100, 50)]
         self.color_players = [QColor(255, 0, 0, 200), QColor(0, 0, 255, 200)]
-    
-    def initLife(self) -> None:
+        self.position = [[QRect(150*j + 100, 100 + 150 * i, 50, 100) for j in range(3)] for i in range(2)] + [[QRect(150*j + 100, 400 + 150 * i, 50, 100) for j in range(3)] for i in range(2)]
+        self.color = [[QColor(255, 0, 0, 200) for j in range(3)] for i in range(2)] + [[QColor(0, 0, 255, 200) for j in range(3)] for i in range(2)]
+        self.grid = [[Placeholder() for j in range(3)] for i in range(2)] + [[Placeholder() for j in range(3)] for i in range(2)]
+        self.players = ("Player1", "Player2")
+        
+        self.initUi() # main window
+        self.initHand(self.current_hand, self.players[0])
+        self.initHand([Placeholder() for i in range(4)], self.players[1])
+
+        self.initLife(9, self.players[0])
+        self.initLife(9, self.players[1])
+
+    # Display player life
+    def initLife(self, number_card: int, player_name: str) -> None:
         # display life points for the 2 players
-        for i in range(2):
-            player = self.players[i]
-            for j in range(player.life.size):
-                card = player.life.container[j]  
-                card.setPixmap(QPixmap("hidden_card.png"))
-                card.move(550, 300*i + 100 + 25*j)
-                card.show()
-    
-    def initHand(self) -> None:
-        # display the hand of the current player
-        player = self.players[self.nb_turns%2] # current player
-        for i in range(len(player.hand.container)):
-            card = player.hand.container[i]  
-            card.setPixmap(QPixmap("monster.png"))
-            card.move(20 + 70*i, 700)
+        i = self.players.index(player_name)
+        for j in range(number_card):
+            card = Card_Graphic(Placeholder(), self.qapp)
+            card.setPixmap(QPixmap("hidden_card.png"))
+            card.move(550, 300*i + 100 + 25*j)
+            if player_name == self.players[self.tour%2]:
+                self.current_life.append(card)
+            else:
+                self.other_life.append(card)
             card.show()
     
-    def clearHand(self) -> None:
-        # erase (graphically) the hand of the current player
-        player = self.players[self.nb_turns%2] # current player
-        print("Main", len(player.hand.container))
-        for i in range(len(player.hand.container)):
-            card = player.hand.container[i]  
-            card.hide()
+    # display the board
+    def initBoard(self, board: list) -> None:
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                self.grid[i][j] = Card_Graphic(board[i][j])
+                self.grid[i][j].move(self.position[i][j])
+                self.grid[i][j].show()
+
+    def initHand(self, hand: list, player_name: str) -> None:
+        # display the hand of a player
+        i = self.players.index(player_name)
+        for j in range(len(hand)):
+            card = Card_Graphic(hand[i], self.qapp)
+            if player_name == self.players[self.tour%2]:
+                card.setPixmap(QPixmap("monster.png"))
+            else:
+                card.setPixmap(QPixmap("hidden_card.png"))
+            card.move(20 + 70*j, 50 + 700*i)
+            card.show()
             
-    def clearLife(self) -> None:
-        # erase (graphically) the life of the current player
-        for i in range(2):
-            player = self.players[i]
-            for j in range(player.life.size):
-                card = player.life.container[j]  
-                card.hide()
+    def initDeck(self, player_name: str) -> None:
+        i = self.players.index(player_name)
+        if player_name == self.players[self.tour%2]:
+            card = self.current_deck
+        else:
+            card = self.other_deck
+        card.setPixmap(QPixmap("monster.png"))
+        card.move(550, 100*i + 25)
+        card.show()
+
+    def clearHand(self, hand: list) -> None:
+        # erase (graphically) the hand of the current player
+        for i in range(len(hand)):
+            card = hand[i]
+            card.hide()
+            card.deleteLater()
     
+    # clear board
+    def clearBoard(self):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                self.grid[i][j].hide()
+                self.grid[i][j].deleteLater()        
+
+    def clearLife(self, player_name: str) -> None:
+        # erase (graphically) the life of a player
+        i = self.players.index(player_name)
+        card = None
+        for j in range(number_card):
+            if player_name == self.players[self.tour%2]:
+                card = self.current_life[j]
+            else:
+                card = self.other_life[j]
+            card.hide()
+            card.deleteLater()
+
+    def clearDeck(self, player_name: str) -> None:
+        if player_name == self.players[self.tour%2]:
+            card = self.current_deck
+        else:
+            card = self.other_deck
+        card.hide()
+
     def initUi(self) -> None:
         # event for changing turns 
         def change_turn():
-            # mettre fin au tour
-            self.nb_actions = 2
-            self.ongo_attack = False
-            self.clearHand()
-            self.clearLife()
-            self.end = self.condition_endgame() # mise a jour de la condition end
-            self.players[self.nb_turns%2].draw_hp()
-            self.nb_turns += 1
-            self.initHand()
-            self.initLife()
+            flag_change_turn = self.game.can_end_turn()
+            if flag_change_turn[0]:
+                self.refresh(flag_change_turn)
+                self.ongo_attack = False
+                # clear hands
+                self.clearHand(self.current_hand)
+                self.clearHand(self.other_hand)
+                
+                self.tour += 1
+                self.end = self.game_condition_endgame()
+                if self.end:
+                    self.qapp.exec_()
+
+            # init hands
+            self.initHand(self.current_hand, self.players[self.tour%2])
+            self.initHand(self.other_hand, self.players[(self.tour+1)%2])
             
-        
+            # init lives
+            number_life = back_
+            self.initLife(number_life, self.players[self.tour%2])
+
+        # button to change turns
         self.finish_turn.move(700, 300)
         self.finish_turn.clicked.connect(change_turn)
-        # main window
-        self.setWindowTitle('GAME')
+
+        # main window with its properties
+        self.setWindowTitle('Sybilian')
         self.setGeometry(50, 50, 900, 900)
-    
-    def condition_endgame(self) -> bool:
-        """
-            return True if a player won,
-            False otherwise
-        """
-        if self.players[0].life_points > 0 and self.players[1].life_points > 0:
-            return False
-        elif self.players[0].life_points == 0:
-            print("Le joueur 2 a gagné!")
-            return True
-        elif self.players[1].life_points == 0:
-            print("Le joueur 1 a gagné!")
-            return True
+
+
+    def refresh(self, list_area: list) -> None:
+        if list_area[1] is not None:
+            self.clearHand(self.current_hand)
+            self.initHand(list_area[1], self.players[self.tour%2])
+        if list_area[2] == 0:
+            self.clearDeck(self.current_deck)
+        if list_area[3] is not None:
+            self.clearLife(self.players[self.tour%2])
+            self.initLife(list_area[3], self.players[self.tour%2])
+        if list_area[5] is not None:
+            self.clearHand(self.other_hand)
+            self.initHand(list_area[5], self.players[(self.tour+1)%2])
+        if list_area[6] == 0:
+            self.clearDeck(self.other_deck)
+        if list_area[7] is not None:
+            self.clearLife(self.players[(self.tour+1)%2])
+            self.initLife(list_area[7], self.players[(self.tour+1)%2])
+        if list_area[9] is not None:
+            self.clearBoard()
+            self.initBoard(list_area[9].grid)
 
     # est-ce qu'on a le droit de prendre cette carte dans la main ?
     def back_take_card_from_hand(self, player : str, index_card : int) -> bool:
         return(True)
 
     def dragEnterEvent(self, e):
-        for i in range(len(self.players[self.nb_turns%2].hand.container)):
-            card = self.players[self.nb_turns%2].hand.container[i]
+        for i in range(len(self.current_hand)):
+            card = self.current_hand[i] # chosen card
             if QRect(card.x(), card.y(), card.width, card.height).contains(e.pos()):
                 self.index_current_card = i
-        if self.back_take_card_from_hand(self.hand[], self.index_current_card):
-            e.accept()
-              
-    def move_card_from_hand_to_board(self, index_current_card : int, position : tuple) -> None:
-        self.players[self.nb_turns%2].hand.container[self.index_current_card].move(self.position[position[0]][position[1]].x(), self.position[position[0]][position[1]].y())
-        self.index_current_card = -1
+                e.accept()
 
-    # est-ce que j'ai le droit de jouer cette carte de ma main ?
-    def back_play_card_from_hand(self, player : str, index_card : int, card : Card, pos : tuple) -> bool:
-        return(True)
+    def move_card_from_hand_to_board(self, index_current_card : int, position : tuple) -> None:
+        self.current_hand[self.index_current_card].move(self.position[position[0]][position[1]].x(), self.position[position[0]][position[1]].y())
+        self.index_current_card = -1
 
     # to play cards on the board (drag'n'drop) --> Jouer des cartes sur le plateau
     def dropEvent(self, e):
         position = e.pos()
-        for i in range(len(self.board.grid)):
-            for j in range(len(self.board.grid[0])):
-                if self.position[i][j].contains(position):
-                    pos = (i, j)
-                    if self.back_play_card_from_hand(self.name_player[self.index_current_card], self.index_current_card) :
-                        self.move_card_from_hand_to_board(self.players[self.nb_turns%2], self.index_current_card, pos)
-                        e.setDropAction(Qt.MoveAction)
-                        e.accept() 
+        flag_spell = self.game.can_play_spell(self.index_current_card)
+        if flag_spell[0]: # spell
+            e.accept()
+            self.refresh(flag_spell)
+        else: # monster
+            for i in range(len(self.grid)):
+                for j in range(len(self.grid[0])):
+                    if self.position[i][j].contains(position):
+                        pos = (i, j)
+                        flag_drag = self.game.can_play_monster(self.index_current_card, pos)
+
+                        if flag_drag[0]:
+                            self.move_card_from_hand_to_board(self.index_current_card, pos)
+                            e.setDropAction(Qt.MoveAction)
+                            e.accept()
+                            self.refresh(flag_drag)
         self.update()
-            
+
     ### GRAPHICS USE ###
-    
+
     # color the areas where players put their cards
     def paintEvent(self, e):
         painter = QPainter(self)
@@ -165,6 +234,9 @@ class Game(QWidget):
                 pen.setWidth(3)
                 painter.setPen(pen)
                 painter.drawRect(self.position[i][j])
+                # restore the initial Alpha mode for card that attack
+                if(self.color[i][j].alpha() == 50):
+                    self.color[i][j].setAlpha(200)
                 painter.fillRect(self.position[i][j], self.color[i][j])
         
         # dessiner les joueurs
@@ -172,54 +244,43 @@ class Game(QWidget):
             painter.fillRect(self.player_medal[i], self.color_players[i])
         painter.end()
 
-    # possibilité d'attaquer via la position sur la grille
-    def back_card_that_attack(self, pos : tuple) -> bool: 
-        return(True)
-    
-    def back_monster_could_be_attacked(self, pos_board : tuple, pos : tuple) -> bool:
-        return(True)
-        
-    def back_player_could_be_attacked(self, pos_board : tuple, player : str) -> bool:
-        return(True)
 
     # attack of a monster --> Attaquer monstres ou joueurs
     def mouseReleaseEvent(self, e):
-        x, y = -1, -1
-        x_att, y_att = -1, -1
         position = e.pos()
-        attack_instance = None
-        attacked_instance = None
-        if self.nb_turns > 1: # pas le premier tour pour qu'on puisse attaquer
+        # draw a card from the deck
+        if QRect(self.current_deck.x(), self.current_deck.y(), self.current_deck.width, self.current_deck.height).contains(e.pos()):
+            flag_draw = self.game.can_draw_card()
+            if flag_draw[0]:
+                self.refresh(flag_draw)
+        
+        # attack mode
+        if self.tour > 1: # pas le premier tour pour qu'on puisse attaquer
             # Y-a-til un clic sur le Board ?
             for i in range(len(self.grid)):
                 for j in range(len(self.grid[0])):
                     if self.position[i][j].contains(position):
                         if not self.ongo_attack: # pas encore d'attaque
-                            x, y = i, j
-                            if self.back_card_that_attack((x, y)):
-                                self.ongo_attack = True
+                            self.i_card_attacking = i
+                            self.j_card_attacking = j
+                            if self.game.card_can_attack((i, j)):
+                                self.ongo_attack = True # ongoing attack
                                 self.color[i][j].setAlpha(50)
-                                self.qapp.setOverrideCursor(Qt.CrossCursor) # curseur cible
+                                self.qapp.setOverrideCursor(Qt.CrossCursor)
                         else: # attaque en cours
-                            x_att, y_att = i, j # coord de la carte attaquée
-                            if self.back_monster_could_be_attacked((x, y), (x_att, y_att)): # monstre peut être attaqué ?                       
-                                self.color[i][j] = QColor(105, 105, 105, 200)
-                            self.players[self.nb_turns%2].attack_monster(self.grid[x][y], self.grid[x_other_player][y_other_player])
+                            flag_attack = self.game.can_attack_monster((self.i_card_attacking, self.j_card_attacking), (i, j))
+                            if flag_attack[0]: # monstre peut être attaqué ?
+                                self.refresh(flag_attack)
+                                if not isinstance(self.grid[i][j].card, Placeholder):
+                                    self.color[i][j] = QColor(105, 105, 105, 200)
+
                             self.ongo_attack = False
-                            
-                            if self.grid[x_other_player][y_other_player].life > 0:
-                                self.color[i][j] = QColor(105, 105, 105, 200)
-                            else:
-                                if self.nb_turns%2 == 0:
-                                    self.color[i][j] = QColor(0, 0, 255, 200)
-                                else:
-                                    self.color[i][j] = QColor(255, 0, 0, 200)
                             self.qapp.restoreOverrideCursor()
-        
-            # Y-a-t-il un clic sur un Player ?
-            for i in range(len(self.player_medal)):
-                if self.player_medal[i].contains(position):
-                    if self.ongo_attack: # attaque en cours
-                        if self.back_player_could_be_attacked(self.player_medal[i]): # joueur peut être attaqué ?
-                            attack_player()
-        self.update()          
+            
+            # attack a player
+            if self.player_medal[(self.tour+1)%2].contains(position):
+                if self.ongo_attack: # attaque en cours
+                    flag_attack_player = self.game.can_attack_opponent_with_monster((self.i_card_attacking, self.j_card_attacking))
+                    if flag_attack_player[0]: # joueur peut être attaqué ?
+                        self.refresh(flag_attack_player)
+        self.update()
